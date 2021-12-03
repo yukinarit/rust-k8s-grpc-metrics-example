a444292a8ccaa3cf0abe02080559dc0bfd4fa25f
Yukinari Tani <yukinarit84@gmail.com>
2021-12-03 23:40:37 +0900

    Add client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use foo::foo_client::FooClient;
+use foo::EchoRequest;
+
+pub mod foo {
+    tonic::include_proto!("foo");
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let mut client = FooClient::connect("http://localhost:20000").await?;
+
+    let request = tonic::Request::new(EchoRequest {
+        name: "Tonic".into(),
+    });
+
+    let response = client.echo(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
 }
