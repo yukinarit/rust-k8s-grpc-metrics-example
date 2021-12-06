781d506bfba2ba7eecfacaf80ff53d87efffd061
Yukinari Tani <yukinarit84@gmail.com>
2021-12-07 01:12:34 +0900

    Instrument metrics

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,9 +5,35 @@ pub mod foo {
     tonic::include_proto!("foo");
 }
 
+#[derive(Debug, serde::Deserialize)]
+struct Config {
+    host: String,
+    port: u16,
+}
+
+impl std::default::Default for Config {
+    fn default() -> Self {
+        Config {
+            host: "localhost".into(),
+            port: 20000,
+        }
+    }
+}
+
+impl Config {
+    fn load() -> Config {
+        let mut cfg = config::Config::default();
+        cfg.merge(config::Environment::with_prefix("client"))
+            .unwrap();
+        cfg.try_into().unwrap_or_else(|_| Default::default())
+    }
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let mut client = FooClient::connect("http://localhost:20000").await?;
+    let cfg = Config::load();
+    println!("{:?}", cfg);
+    let mut client = FooClient::connect(format!("http://{}:{}", cfg.host, cfg.port)).await?;
 
     let request = tonic::Request::new(EchoRequest {
         name: "Tonic".into(),
@@ -15,7 +41,7 @@ async fn main() -> anyhow::Result<()> {
 
     let response = client.echo(request).await?;
 
-    println!("RESPONSE={:?}", response);
+    println!("Response={:?}", response);
 
     Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,36 @@
+use metrics::{histogram, increment_counter, increment_gauge};
+use std::time::Instant;
 use tonic::{transport::Server, Request, Response, Status};
 
 use foo::foo_server::{Foo, FooServer};
 use foo::{EchoReply, EchoRequest};
 
 pub mod foo {
-    tonic::include_proto!("foo"); // The string specified here must match the proto package name
+    tonic::include_proto!("foo");
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct Config {
+    host: String,
+    port: u16,
+}
+
+impl std::default::Default for Config {
+    fn default() -> Self {
+        Config {
+            host: "0.0.0.0".into(),
+            port: 20000,
+        }
+    }
+}
+
+impl Config {
+    fn load() -> Config {
+        let mut cfg = config::Config::default();
+        cfg.merge(config::Environment::with_prefix("server"))
+            .unwrap();
+        cfg.try_into().unwrap_or_else(|_| Default::default())
+    }
 }
 
 #[derive(Debug, Default)]
@@ -13,11 +39,19 @@ pub struct FooHandler {}
 #[tonic::async_trait]
 impl Foo for FooHandler {
     async fn echo(&self, request: Request<EchoRequest>) -> Result<Response<EchoReply>, Status> {
+        let now = Instant::now();
+
         println!("Got a request: {:?}", request);
 
         let reply = foo::EchoReply {
             message: request.into_inner().name.clone(),
         };
+
+        histogram!("foo_histgram", now.elapsed(), "api" => "echo", "result" => "ok");
+
+        increment_counter!("foo_counter");
+
+        increment_gauge!("foo_gauge", 10.0);
 
         Ok(Response::new(reply)) // Send back our formatted greeting
     }
@@ -25,8 +59,13 @@ impl Foo for FooHandler {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let addr = "0.0.0.0:20000".parse()?;
+    let cfg = Config::load();
+    println!("{:?}", cfg);
+
+    let addr = format!("{}:{}", cfg.host, cfg.port).parse()?;
     let foo = FooHandler::default();
+
+    metrics_exporter_prometheus::PrometheusBuilder::new().install()?;
 
     Server::builder()
         .add_service(FooServer::new(foo))
